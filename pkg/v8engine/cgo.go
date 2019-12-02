2d489ed0f19f95de046bd99881b8ba716aef1722
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-12-02 19:23:26 +0000

    Add back cgo compile flags

---
+++ pkg/v8engine/cgo.go
@@ -0,0 +1,7 @@
+package v8engine
+
+// #cgo CXXFLAGS: -fno-rtti -fpic -Ideps/include
+// #cgo LDFLAGS: -pthread -lv8
+// #cgo darwin LDFLAGS: -Ldeps/darwin-x86_64
+// #cgo linux LDFLAGS: -Ldeps/linux-x86_64
+import "C"
