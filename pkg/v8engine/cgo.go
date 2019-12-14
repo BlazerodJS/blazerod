126ed4a428a3a0812a6465a7387503fb8e615279
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-12-14 16:04:55 +0000

    Remove Bazel for builds

--- pkg/v8engine/cgo.go
+++ pkg/v8engine/cgo.go
@@ -1,6 +1,6 @@
 package v8engine
 
-// #cgo CXXFLAGS: -fno-rtti -fpic -Ideps/include
+// #cgo CXXFLAGS: -fno-rtti -fpic -Ideps/include -std=c++11
 // #cgo LDFLAGS: -pthread -lv8
 // #cgo darwin LDFLAGS: -Ldeps/darwin-x86_64
 // #cgo linux LDFLAGS: -Ldeps/linux-x86_64
